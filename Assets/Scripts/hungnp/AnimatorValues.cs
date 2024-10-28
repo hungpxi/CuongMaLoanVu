fdddbc5ac12344522e6a6c7c33e0103a99c7ec4d
hungnphe173547 <hungnphe173547@fpt.edu.vn>
2024-10-28 13:32:08 +0700

    update move

---
+++ Assets/Scripts/hungnp/AnimatorValues.cs
@@ -0,0 +1,52 @@
+
+namespace SHG.AnimatorCoder
+{
+    /// <summary> Complete list of all animation state names </summary>
+    public enum Animations
+    {
+        //Change the list below to your animation state names
+        Idle,
+        Walk,
+        Run,
+        Attack1,
+        Attack2,
+        Attack3,
+        Hurt,
+        Block,
+        IdleBlock,
+        Spell,
+        SpellSlam,
+        Transformation,
+        Jump,
+        Fall,
+        JumpAttack,
+        Death,
+        TransformationNormal,
+        Ice_Idle,
+        Ice_Walk,
+        Ice_Run,
+        Ice_Hurt,
+        Ice_Death,
+        Ice_Jump,
+        Ice_Fall,
+        Ice_Attack1,
+        Ice_Attack2,
+        Ice_Attack3,
+        Ice_Block,
+        Ice_IdleBlock,
+        Ice_SpellSlam,
+        Ice_Spell,
+        Ice_JumpAttack,
+        RESET  //Keep Reset
+    }
+
+    /// <summary> Complete list of all animator parameters </summary>
+    public enum Parameters
+    {
+        //Change the list below to your animator parameters
+        GROUNDED,
+        FALLING
+    }
+}
+
+
--- Assets/Scripts/hungnp/PlayerController.cs
+++ Assets/Scripts/hungnp/PlayerController.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class PlayerController : MonoBehaviour
+using SHG.AnimatorCoder;
+public class PlayerController : AnimatorCoder
 {
     public float moveSpeed;
     public float jumpForce;
+    float horizonMove = 0f;
 
     //player movement
     public KeyCode left;
@@ -20,6 +21,7 @@ public class PlayerController : MonoBehaviour
     public KeyCode attackO;
 
     private Rigidbody2D rb;
+    private SpriteRenderer sprite;
 
     //groundCheck
     public Transform groundCheck;
@@ -27,44 +29,95 @@ public class PlayerController : MonoBehaviour
     public LayerMask groundMask;
     public bool isGround;
 
+    public static PlayerController instance;
 
+    private void Awake()
+    {
+        instance = this;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
+        Initialize();
         rb = gameObject.GetComponent<Rigidbody2D>();
+        sprite = gameObject.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        isGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundMask);
-        if(Input.GetKey(left))
+        DefaultAnimation(0);
+        // horizonMove = Input.GetAxisRaw("Horizontal");
+        if (Input.GetKey(left))
         {
             if (transform.localScale.x > 0)
             {
                 transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
             }
+
             rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
         }
-        else if(Input.GetKey(right))
+        else if (Input.GetKey(right))
         {
             if (transform.localScale.x < 0)
             {
                 transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
             }
+
             rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
         }
         else
         {
             rb.velocity = new Vector2(0, rb.velocity.y);
         }
+        isGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundMask);
 
         if (Input.GetKeyDown(jump) && isGround)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-
+            Play(new(Animations.Jump));
         }
 
     }
+
+    //private void FixedUpdate()
+    //{
+    //    rb.velocity = new(horizonMove * moveSpeed, rb.velocity.y);
+    //}
+
+    public override void DefaultAnimation(int layer)
+    {
+        if(horizonMove == 0) Play(new(Animations.Idle));
+        else Play(new(Animations.Run));
+        //if (horizonMove != 0) sprite.flipX = horizonMove < 0;
+        if (GetCurrentAnimation(0) != Animations.Attack1 && horizonMove != 0) sprite.flipX = horizonMove < 0;
+    }
+
+    private readonly AnimationData IDLE = new(Animations.Idle);
+    private readonly AnimationData RUN = new(Animations.Run);
+
 }
+
+//if (Input.GetKey(left))
+//{
+//    if (transform.localScale.x > 0)
+//    {
+//        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+//    }
+
+//    rb.velocity = new Vector2(-moveSpeed * horizonMove, rb.velocity.y);
+//}
+//else if (Input.GetKey(right))
+//{
+//    if (transform.localScale.x < 0)
+//    {
+//        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+//    }
+
+//    rb.velocity = new Vector2(moveSpeed * horizonMove, rb.velocity.y);
+//}
+//else
+//{
+//    rb.velocity = new Vector2(0, rb.velocity.y);
+//}
